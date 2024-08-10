4b0191f65afde3d082f0dff103a03dd393e29404
mgouveas <mgsandmgs@gmail.com>
2024-08-10 14:24:42 -0300

    feat: implements links repository with tests

---
+++ src/models/repositories/links_repository_test.py
@@ -0,0 +1,35 @@
+import pytest # type: ignore
+import uuid
+from src.models.settings.db_connection_handler import db_connection_handler
+from .links_repository import LinksRepository
+
+db_connection_handler.connection()
+trip_id = str(uuid.uuid4())
+link_id = str(uuid.uuid4())
+
+# @pytest.mark.skip(reason='interacao com o banco')
+def test_registry_links():
+    conn = db_connection_handler.get_connection()
+    links_repository = LinksRepository(conn)
+
+    link_infos = {
+        "id": link_id,
+        "trip_id": trip_id,
+        "link": "https://www.gol.com",
+        "title": "Voo"
+    }
+
+    links_repository.registry_link(link_infos)
+
+# @pytest.mark.skip(reason='interacao com o banco')
+def test_find_links_from_trip():
+    conn = db_connection_handler.get_connection()
+    links_repository = LinksRepository(conn)
+
+    links = links_repository.find_links_from_trip(trip_id)
+
+    assert isinstance(links, list)
+    assert isinstance(links[0], tuple)
+
+    print()
+    print(links)
