176cc0b5faf3965d5972f26cd4ad82bf687a0777
mgouveas <mgsandmgs@gmail.com>
2024-08-10 11:49:12 -0300

    feat:Adding tests to the project

--- src/models/repositories/trips_repository_test.py
+++ src/models/repositories/trips_repository_test.py
@@ -1,3 +1,4 @@
+import pytest # type: ignore
 import uuid
 from .trips_repository import TripsRepository
 from src.models.settings.db_connection_handler import db_connection_handler
@@ -6,6 +7,7 @@ from datetime import datetime, timedelta
 db_connection_handler.connection()
 trip_id = str(uuid.uuid4())
 
+@pytest.mark.skip(reason='interacao com o banco')
 def test_create_trip():
     conn = db_connection_handler.get_connection()
     trips_repository = TripsRepository(conn)
@@ -21,12 +23,14 @@ def test_create_trip():
 
     trips_repository.create_trip(trips_infos)
 
+@pytest.mark.skip(reason='interacao com o banco')
 def test_find_trip_by_id():
     conn = db_connection_handler.get_connection()
     trips_repository = TripsRepository(conn)
 
     trip = trips_repository.find_trip_by_id(trip_id)
 
+@pytest.mark.skip(reason='interacao com o banco')
 def test_update_trip_status():
     conn = db_connection_handler.get_connection()
     trips_repository = TripsRepository(conn)
