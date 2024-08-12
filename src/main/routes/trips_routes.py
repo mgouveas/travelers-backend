4dd2a0497431e5e0a955cae65b44f7e467bad6ab
mgouveas <mgsandmgs@gmail.com>
2024-08-11 22:10:09 -0300

    fix: adjustment in db

--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -6,14 +6,20 @@ trips_routes_bp = Blueprint("trip_routes", __name__)
 from src.controllers.trip_creator import TripCreator
 from src.controllers.trip_finder import TripFinder
 from src.controllers.trip_confirmer import TripConfirmer
+
 from src.controllers.link_creator import LinkCreator
 from src.controllers.link_finder import LinkFinder
+
+from src.controllers.participants_creator import ParticipantCreator
+
+from src.controllers.activities_creator import ActivityCreator
 
 # Importação de repositórios
 from src.models.repositories.trips_repository import TripsRepository
 from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
 from src.models.repositories.links_repository import LinksRepository
-
+from src.models.repositories.participants_repository import ParticipantsRepository
+from src.models.repositories.activities_repository import ActivitiesRepository
 
 # Importação das conexões
 from src.models.settings.db_connection_handler import db_connection_handler
@@ -63,6 +69,7 @@ def create_trip_link(tripId):
 
     return jsonify(response["body"]), response["status_code"]
 
+
 @trips_routes_bp.route("/trips/<tripId>/links", methods=["GET"])
 def find_trip_link(tripId):
     conn = db_connection_handler.get_connection()
@@ -70,5 +77,28 @@ def find_trip_link(tripId):
     controller = LinkFinder(links_repository)
 
     response = controller.find(tripId)
+
+    return jsonify(response["body"]), response["status_code"]
+
+
+@trips_routes_bp.route("/trips/<tripId>/invites", methods=["POST"])
+def invite_to_trip(tripId):
+    conn = db_connection_handler.get_connection()
+    participants_repository = ParticipantsRepository(conn)
+    emails_repository = EmailsToInviteRepository(conn)
+    controller = ParticipantCreator(participants_repository, emails_repository)
+
+    response = controller.create(request.json, tripId)
+
+    return jsonify(response["body"]), response["status_code"]
+
+
+@trips_routes_bp.route("/trips/<tripId>/activities", methods=["POST"])
+def create_activity(tripId):
+    conn = db_connection_handler.get_connection()
+    activities_repository = ActivitiesRepository(conn)
+    controller = ActivityCreator(activities_repository)
+
+    response = controller.create(request.json, tripId)
 
     return jsonify(response["body"]), response["status_code"]
